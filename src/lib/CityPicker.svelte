09defcdce4d541f097a1b7deda797f7cae295f8f
Rahul Bhargava <rahulbot@gmail.com>
2025-03-25 10:30:45 -0400

    first commit, doesn't really work

---
+++ src/lib/CityPicker.svelte
@@ -0,0 +1,28 @@
+<script>
+	let {changeChoice, choices} = $props();
+	
+	//const properties = $props();
+	//const choices = properties.choices;
+	//let picked = $bindable(properties.picked);
+
+</script>
+
+<div class="picker">	
+	<nav>
+		{#each choices as item}
+			<a href="#{item}" onclick={() => changeChoice(item)}>{item}</a>
+		{/each}
+	</nav>
+</div>
+	
+<style>
+	.picker {
+		display: inline-block;
+		border: 1px solid #666;
+		padding: 10px;
+	}
+	a {
+		display: inline-block;
+		padding: 0px 10px;
+	}
+</style>
